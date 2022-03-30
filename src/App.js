5e70061869300063c8aa66429f5479a20ac90af3
Malte N <62905344+mltn123@users.noreply.github.com>
2022-03-30 09:20:13 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+import Flow from "./Flow";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Flow />
+    </div>
+  );
+}
